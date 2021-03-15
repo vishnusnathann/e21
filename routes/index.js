d6f70b3772e07d0af8708aa47285d9b7f17a72b7
vishnath <vishnusnathan@gmail.com>
2021-03-15 16:16:14 +0530

    Added basic vote add aoi

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const voteRoutes = require('./voteRoutes');
 const router = express.Router(); 
 
-
+router.use('/vote',voteRoutes);
 
 module.exports = router;
--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -1,4 +1,26 @@
 const express = require('express');
-const authRoutes = express.Router();  
+const pool = require('../config/db');
+const Vote = require('../models/Vote');
+const voteRoutes = express.Router();
 
+
+voteRoutes.post("/add_vote",async (req,res) => {
+    
+    let {voter_id,party_id,alliance_id,constituency_id,
+        candidate_name,datetime,browser_namr,os_name,os_version,
+        device_platform,voter_ip} = req.body;
+
+        console.log(req.body);
+
+    try {
+		pool.query(Vote.addVote(),[voter_id,party_id,alliance_id,constituency_id,
+            candidate_name,datetime,browser_namr,os_name,os_version,
+            device_platform,voter_ip]).then((response)=>{
+                res.status(200).json({type:'success'})
+            });
+	} catch (err) {
+		console.log(err);
+	}
+    
+})
 module.exports =  voteRoutes;
