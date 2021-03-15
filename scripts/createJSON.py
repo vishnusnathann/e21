ea219e1495632b2ec91450893cce8e6707cdb898
jerin joseph <123jerin@gmail.com>
2021-03-15 19:06:47 +0530

    createpartyJSON funcionality & improved data.json

--- scripts/createJSON.py
+++ scripts/createJSON.py
@@ -9,6 +9,10 @@ f=open('data.txt',"r")
 data=f.read().split("-")
 #print(data)
 # print(type(data))
+partyfile=open('partyData.json',"r")
+partyJSON=partyfile.read().lower()
+party = json.loads(partyJSON)
+print(party)
 
 
 lst=[]
@@ -32,13 +36,25 @@ for item in lst:
     if(len(item)>1):
         constituency={}
         constituency['constituency_id']=item[0]
-        constituency['constituency_name']=item[1]
+        constituency_name=item[1].split(")")[-1:][0].strip()
+        #print(constituency_name)
+        constituency['constituency_name']=constituency_name
         candidates=[]
         #print(item[0])
         for i in range(2,len(item),2):
             candidate={}
             #print("party_name: ",item[i])
-            candidate['party_name']=item[i]
+            party_data=item[i].split(" ")
+            party_name=" ".join(party_data[:-1]).strip()
+            party_code="".join(party_data[-1:]).strip()
+            candidate['party_name']=party_name
+            candidate['party_code']=party_code
+            if(party_name.lower() in party):
+                #print(party_name)
+                candidate['party_id']=party[party_name.lower()]
+            else:
+                candidate['party_id']=-1
+            #print(item[i],":",candidate['party_name'],":",candidate['party_code'])
             if(i+1<len(item)):
                 #print("candidate_name: ",item[i+1])
                 candidate['candidate_name']=item[i+1]
