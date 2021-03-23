c055a452d6ac05d82b3bba042a080c3c84a995fa
vishnath <vishnusnathan@gmail.com>
2021-03-23 12:56:24 +0530

    user Ip address request variable added in routes

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -15,33 +15,29 @@ const csrfProtection = csrf({ cookie: true })
 
 
 voteRoutes.get('/csrf', csrfProtection, function (req, res) {
-
-    console.log(req.connection.remoteAddress);
     
     res.status(200).json({ csrfToken: req.csrfToken() });
 })
 
 voteRoutes.post("/add_vote" ,async (req,res) => {
 
-
-
     
     let {voter_id,party_id,alliance_id,constituency_id,
         candidate_name,datetime,browser_name,os_name,os_version,
-        device_platform} = req.body;
+        device_platform,voter_ip} = req.body;
 
 
         console.log(req.body);
 
     let voter_uid = hash({
         voter_id:voter_id,
-        ip:req.connection.remoteAddress
+        ip:voter_ip
     })
 
     try {
 		pool.query(Vote.addVote(),[voter_uid,party_id,alliance_id,constituency_id,
             candidate_name,datetime,browser_name,os_name,os_version,
-            device_platform,req.connection.remoteAddress]).then((response)=>{
+            device_platform,voter_ip]).then((response)=>{
                 res.status(200).json({type:'success'})
             });
 	} catch (err) {
