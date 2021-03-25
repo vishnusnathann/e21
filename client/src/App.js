90f5357227f2e693c66c9740f6a32d8fcabe46e7
vishnath <vishnusnathan@gmail.com>
2021-03-25 08:21:31 +0530

    Production ready for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import  { Suspense, lazy } from 'react';
 import './App.css';
-
-import ConstituencySelect from './Components/ContituencySelect/ConstituencySelect';
-import VotingMachine from './Components/VotingMachine/VotingMachine';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import FinalAnnotation from './Components/FinalAnnotation/FinalAnnotation';
-import Navbar from './Components/Navbar/Navbar';
+import Loader from './Components/Loader/Loader';
+
+const ConstituencySelect = lazy(() => import('./Components/ContituencySelect/ConstituencySelect'));
+const VotingMachine = lazy(() => import('./Components/VotingMachine/VotingMachine'));
+const FinalAnnotation = lazy(() => import('./Components/FinalAnnotation/FinalAnnotation'));
+const Navbar = lazy(() => import ('./Components/Navbar/Navbar'));
+
+
 
 function App() {
 
@@ -16,30 +20,32 @@ const [constituencySelectFlag, setConstituencySelectFlag] = useState(false);
 
 
 return (
-	<div className="App" >
 
-		<Router>
-			<Navbar/>
-			<Switch>
-				<Route  path="/"  exact render={props => 
-					<ConstituencySelect {...props} selectedConstituency={selectedConstituency} 
-						setSelectedConstituency={setSelectedConstituency} 
-						setConstituencySelectFlag={setConstituencySelectFlag} 
+	<Suspense fallback={<Loader/>}>
+		<div className="App" >
+			<Router>
+				<Navbar/>
+				<Switch>
+					<Route  path="/"  exact render={props => 
+						<ConstituencySelect {...props} selectedConstituency={selectedConstituency} 
+							setSelectedConstituency={setSelectedConstituency} 
+							setConstituencySelectFlag={setConstituencySelectFlag} 
+						/>
+						}
 					/>
-					}
-				/>
-				
-				<Route path="/vote"  exact render={props => 
-					<VotingMachine {...props} selectedConstituency={selectedConstituency}/>}
-				/>
+					
+					<Route path="/vote"  exact render={props => 
+						<VotingMachine {...props} selectedConstituency={selectedConstituency}/>}
+					/>
 
-				<Route path="/vote_casted"  exact render={props => 
-					<FinalAnnotation {...props} />}
-				/>
-				<Redirect to="/"/>
-			</Switch>
-		</Router>
-	</div>
+					<Route path="/vote_casted"  exact render={props => 
+						<FinalAnnotation {...props} />}
+					/>
+					<Redirect to="/"/>
+				</Switch>
+			</Router>
+		</div>
+	</Suspense>
 );
 }
 
